acf704d6297fd881ee4a4ae9417d450b4b162d18
Rudi Grinberg <rudi.grinberg@gmail.com>
2017-04-12 15:01:59 -0400

    Test how gencodes are generated (#3)
    * Move code generation into a static method
    
    Or pure function. Depending on what you like to call it
    
    * Add simple test case to generateCodes
    
    * Remove mockito noise
    
    * Test the garden path
    
    Prefix: C
    Suffix: Null
    Delimiter: Null

---
+++ src/test/java/org/protege/editor/owl/server/http/handlers/CodeGenHandlerTest.java
@@ -0,0 +1,35 @@
+package org.protege.editor.owl.server.handlers;
+
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.protege.editor.owl.server.http.handlers.CodeGenHandler;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.when;
+
+public class CodeGenHandlerTest {
+
+    @Test
+    public void generateNTokens() {
+        List<String> codes = CodeGenHandler.generateCodes(10, 5, "p", "s", "_");
+        List<String> expected = Arrays.asList("p_10_s",
+                                              "p_11_s",
+                                              "p_12_s",
+                                              "p_13_s",
+                                              "p_14_s");
+        assertThat(codes, is(expected));
+    }
+
+    @Test
+    public void generateTokensNulls() {
+        List<String> codes = CodeGenHandler.generateCodes(9, 3, "C", null, null);
+        List<String> expected = Arrays.asList("C9", "C10", "C11");
+        assertThat(codes, is(expected));
+    }
+}
