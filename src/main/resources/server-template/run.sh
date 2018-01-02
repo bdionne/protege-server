411b7d4bc204f58a0d6b2dcf7d686426ca72f576
Bob Dionne <dionne@dionne-associates.com>
2018-01-02 14:47:43 -0500

    WIP - parameterize build for Jenkins

--- src/main/resources/server-template/run.sh
+++ src/main/resources/server-template/run.sh
@@ -2,7 +2,7 @@
 
 cd `dirname $0`
 
-java -Xmx4000M -Xms1500M \
+java -X${java.mem.mx} -Xms1500M \
      -Dorg.protege.owl.server.configuration=server-configuration.json \
      -cp "bundles/*" \
      org.protege.editor.owl.server.http.HTTPServer
